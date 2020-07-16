21ef5e17c3f1d1952829f6e85d93b45006178f8e
TsuruharaKota <m1830@s.akashi.ac.jp>
2020-07-16 21:29:54 +0900

    add func

--- GA_main.cpp
+++ GA_main.cpp
@@ -3,6 +3,7 @@
 #include<GA.h>
 #include<algorithm>
 #include<array>
+#include<utility>
 
 constexpr int GENOM_LENGTH = 100;
 constexpr int MAX_GENOM_LIST = 100;
@@ -41,8 +42,48 @@ Genom& select(Genom(&ga)[num], elite_length){
 }
 
 //交叉関数
-Genom& crossover(ga_first, ga_second){
+void crossover(Genom &ga[2]){
     Genom child_genom();
     //二点交叉を行う
+    std::mu19937 mt{std::random_device{}()};
+    std::array<std::uniform_int_distribution<int>, 2> dist(0, 100);
+    int range_first = dist[0](mt), range_second = dist[1](mt);
+    range_first > range_second ? std::swap(range_first, range_second) : range_first = range_first;
+    std::array<std::array<int, range_second - range_first>, 2> crossover_body{};
+    std::array<std::array<int, 100>, 2> genom_list = ga.getGenom();
+    //二点間のの遺伝子を抽出
+    for(int i = 0; i < 2; ++i){
+        for(int k = 0; k < (range_second - range_first), ++k){
+            crossover_body[i][k] = ga[i][k]; 
+        }
+    }
+    //交叉
+    for(int k = 0; k < (range_second - range_first), ++k){
+        ga[0][k] = crossover_body[1][k];
+        ga[1][k] = crossover_body[0][k];
+    }
 }
 
+Genom& nextGeneration(Genom &ga, const Genom &ga_elite, const Genom &ga_progeny){
+    //エリート主義
+    //現行世代個体集団の評価を低い順にソートする
+    std::sort(ga.begin(), ga.end(), [](const Genom& lGenom, const Genom& rGenom){return lGenom > rGenom;})
+    std::array<Genom next_generation, ga.size()> = ga;
+    //追加するエリート集団と子孫集団の合計分を取り除く
+    int count{}
+    for(count = 0; count < ga_elite.size(); ++count){
+        next_generation[i].setGenom(ga_elite[count].getGenom());
+    }
+    for(; count < (ga_elite.size() + ga_progeny.size()); ++count){
+        next_generation[i].setGenom(ga_progeny[count - ga_elite.size().getGenom()]);
+    }
+    //このままでは返せないstd::unique_ptrを使う
+    return next_generation;
+}
+
+//突然変異関数(未実装)
+void mutation(Genom &ga, double individual_mutation, double genom_mutation){}
+
+int main(){
+    
+}
